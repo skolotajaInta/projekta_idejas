b96d34413c2912329666cac74b7cd6503f616178
skolotajaInta <programmesanas.video@gmail.com>
2022-01-12 15:08:39 +0200

    papildinājums Cpp failam

--- CppPiemers.cpp
+++ CppPiemers.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
+	int a=3, b=7;
 	cout<<"Sveiki!\n";
 	cout<<"Meginam so programmu izmitinat GitHub makoni.\n";
+	cout<<a<<"+"<<b<<"="<<a+b;
 }
